db6ab8fe4eb50bf4b1606369b1bf2969188d331e
SUJAY S <44267241+sujay12345@users.noreply.github.com>
2020-10-25 12:03:23 +0530

    Update settings.py

--- Finalproject/settings.py
+++ Finalproject/settings.py
@@ -76,13 +76,15 @@ WSGI_APPLICATION = 'Finalproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+DATABASES = {                                                                    
+    'default': {                                                                 
+        'ENGINE': 'django.db.backends.postgresql',                               
+        'NAME':'pollsdb',                                            
+        'HOST': 'smellfactory.postgres.database.azure.com',                                            
+        'USER': 'sujaysreedhar@smellfactory',                                            
+        'PASSWORD':'Ujay6674@1234'                                         
+    }                                                                            
 }
-
 AUTH_USER_MODEL = 'smellfactory.User'
 
 # Password validation
